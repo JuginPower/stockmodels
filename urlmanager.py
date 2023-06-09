02ff655d3b0b47f1074bc6870e3c8e4e8846d3cf
JuginPower <juginpower90@gmail.com>
2023-06-09 21:15:27 +0200

    feature-indizmeta: urlmanager refactored

--- urlmanager.py
+++ urlmanager.py
@@ -6,10 +6,22 @@ class Url(Datamanager):
     def __init__(self, indiz_id):
         super().__init__()
         self.fk_id = indiz_id
-        self._url = [row[0] for row in self.select(f"SELECT url FROM indiz_url WHERE indiz_id={self.fk_id} AND active=1")]
+        self._url = [list(row) for row in self.select(f"SELECT url, selector, active FROM indiz_url WHERE indiz_id={self.fk_id}")]
 
-    def get_url(self):
-        return self._url
+    def get_url(self, active=False):
+        if active:
+            return [row[0] for row in self._url if row[-1]==active]
+        else:
+            return [row[0] for row in self._url]
 
-    def get_url_active_items(self):
-        return [list(row) for row in self.select(f"SELECT url, selector FROM indiz_url WHERE indiz_id={self.fk_id} AND active=1")]
+    def get_url_selector(self, active=False):
+        if active:
+            return [list(row[0:2]) for row in self._url if row[-1]==active]
+        else:
+            return [list(row[0:2]) for row in self._url]
+
+    def get_all_items(self, active=False):
+        if active:
+            return [list(row[0:]) for row in self._url if row[-1]==active]
+        else:
+            return [list(row[0:]) for row in self._url]
